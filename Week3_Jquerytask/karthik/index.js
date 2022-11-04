5a4184520f4fdc9e7b5edc13c37dee7228abd331
karthik1758 <chakalikarthik2001@gmail.com>
2022-11-04 17:07:51 +0530

    task file uploaded

---
+++ Week3_Jquerytask/karthik/index.js
@@ -0,0 +1,16 @@
+window.onload = function () {
+    var today = new Date();
+    var month = today.toLocaleString("default", { month: "short" });
+    var date = today.getDate() + "-" + month + "-" + today.getFullYear();
+    var hours = today.getHours();
+    var minutes = today.getMinutes();
+    var ampm = hours >= 12 ? "pm" : "am";
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    var strTime = hours + ":" + minutes + " " + ampm;
+    //var dateTime = date+' '+time;
+  
+    document.getElementById("displayDateTime").innerHTML =
+      date + "<br>" + strTime;
+  };
