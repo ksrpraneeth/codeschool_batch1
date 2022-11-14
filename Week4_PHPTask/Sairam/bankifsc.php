8ec2006a45e23c4c853951b05d89c590b052da59
Sairamlucky <nagasairamsela@gmail.com>
2022-11-14 17:17:57 +0530

    task uploaded

---
+++ Week4_PHPTask/Sairam/bankifsc.php
@@ -0,0 +1,35 @@
+<?php
+ $resultArray = [
+
+ 
+];
+ if (!array_key_exists("bankifsccode", $_POST)) {
+
+array_push($resultArray,  "Please enter ifsccode");
+
+ }
+ $bankifsccode = $_POST['bankifsccode'];
+
+ if (!preg_match("/[A-Z]{4}[0][0-9]{6}/", $bankifsccode)) {
+ //array_push($resultArray, "ifsccode should be 11digits");
+    $resultArray = [
+            "Errors" => "Invalid code",
+            "bankName" => "",
+            "bankbranch" => ""];
+        }
+ if (preg_match("/[A-Z]{4}[0][0-9]{6}/", $bankifsccode)) {
+ //array_push($resultArray, "ifsccode should be 11digits");
+    $resultArray = [
+            "Errors" => "",
+            "bankName" => "icici",
+            "bankbranch" => "bengalore"];
+        }
+ if (preg_match("/[A-Z]{4}[0][0-9]{6}/", $bankifsccode)) {
+ //array_push($resultArray, "ifsccode should be 11digits");
+    $resultArray = [
+            "Errors" => "",
+            "bankName" => "SBI",
+            "bankbranch" => "bengalore"];
+        }
+     echo json_encode($resultArray);
+ ?>
---
+++ Week4_PHPTask/Sairam/validate.js
@@ -0,0 +1,110 @@
+$("document").ready(function () {
+  $("#submit").click(function () {
+    // Input data
+
+    let partyAccountnumber = $("#partyAccountNumber").val()
+    let confirmpartyAccountNumber = $("#confirmpartyAccountNumber").val()
+    let partyName = $("#partyName").val()
+    let purpose = $("#purpose").val()
+    let bankifsccode = $("#bankifsccode").val()
+    let headofAccount = $("#headofAccount").val()
+     let Expendituretype = $("#Expendituretype").val()
+     let purposetype= $("#Purposetype").val()
+    // let partyAmount = $("#partyAmount").val()
+
+    // AJAX Call
+    $.ajax({
+      url: "validation.php",
+      method: "POST",
+      datatype: JSON,
+      data: {
+        partyAccountnumber,
+        confirmpartyAccountNumber,
+        partyName,
+
+      },
+
+      success: (data) => {
+
+        data = JSON.parse(data);
+        console.log(data)
+        $("#errorsid").text(data.messagesArray);
+
+      },
+
+    });
+
+  });
+
+});
+
+
+$("document").ready(function () {
+  $("#search").click(function () {
+    //input data
+    let bankifsccode = $("#bankifsccode").val()
+    $.ajax({
+      url: "bankifsc.php",
+      method: "POST",
+      data: {
+        bankifsccode,
+      },
+
+      success: (data) => {
+
+        data = JSON.parse(data);
+
+        $("#errorsid").text(data.Errors);
+        $("#bankname").text(data.bankName);
+        $("#bankbranch").text(data.bankbranch);
+      },
+
+
+    });
+
+  });
+});
+
+
+
+//input data
+$("#headofAccount").change(function () {
+  let headofAccount = $("#headofAccount").val()
+  $.ajax({
+    url: "headofaccount.php",
+    method: "POST",
+    data: {
+      headofAccount,
+    },
+    success: (data) => {
+      data = JSON.parse(data);
+      console.log(data)
+      $("#balance").text(data.balance);
+      $("#Loc").text(data.Loc);
+    },
+
+  });
+
+});
+
+//input data
+("#Expendituretype").change(function(){
+  let Expendituretype = $("#Expendituretype").val()
+  $.ajax({
+      url : "Expendituretype.php",
+      method:"POST",
+      
+      data:{
+        Expendituretype,
+      },
+      success:(data) => {
+           data = JSON.parse(data);
+           var ar="";
+           console.log(data.Purposetype)
+           for(var i = 0; i < data.length; i++) {
+            ar += "<option value='" + data[i] + "'>" + data[i] + "</option>";
+        }
+        $("select['#Purposetype']").append('<option>'+data[i]+'</option');
+      }
+      })
+    })
