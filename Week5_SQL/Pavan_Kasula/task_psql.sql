468af7ca25e66d3ab040992cc5915667a4a725bc
PavanSaiKasula <pavanpixelvide@gmail.com>
2022-11-18 23:24:51 +0530

    UPDATED-1

--- Week5_SQL/Pavan_Kasula/task_psql.sql
+++ Week5_SQL/Pavan_Kasula/task_psql.sql
@@ -1,110 +1,13 @@
+--> Step-1
 -->Creating Database
+
 CREATE DATABASE HOTEL_PIXEL;
 
-CREATE TABLE Booking_Details(
-    Booking_ID VARCHAR(10) PRIMARY KEY,
-    Guest_ID VARCHAR(25) REFERENCES Guest_Detials(Guest_ID),
-    Room_No VARCHAR(40) REFERENCES Room_Type(Room_No),
-    Staff_ID VARCHAR(25) REFERENCES Staff_Details(Staff_ID),
-    Arrival_Date DATE,
-    Departurte_Date DATE,
-    Payment_ID VARCHAR(25) REFERENCES Payment_Details(Payment_ID)
-);
-
-INSERT INTO
-    Booking_Details
-VALUES
-(
-        'B101',
-        'G01',
-        '101',
-        'S02',
-        '2022-05-15',
-        '2022-05-17',
-        'P-0012'
-    );
-
-INSERT INTO
-    Booking_Details
-VALUES
-(
-        'B102',
-        'G02',
-        '201',
-        'S04',
-        '2022-05-16',
-        '2022-05-18',
-        'P-0013'
-    );
-
-INSERT INTO
-    Booking_Details
-VALUES
-(
-        'B103',
-        'G03',
-        '401',
-        'S06',
-        '2022-05-16',
-        '2022-05-20',
-        'P-0014'
-    );
-
-INSERT INTO
-    Booking_Details
-VALUES
-(
-        'B104',
-        'G04',
-        '202',
-        'S01',
-        '2022-05-17',
-        '2022-05-22',
-        'P-0015'
-    );
-
-INSERT INTO
-    Booking_Details
-VALUES
-(
-        'B105',
-        'G05',
-        '301',
-        'S04',
-        '2022-05-17',
-        '2022-05-19',
-        'P-0016'
-    );
-
-INSERT INTO
-    Booking_Details
-VALUES
-(
-        'B106',
-        'G06',
-        '402',
-        'S03',
-        '2022-05-18',
-        '2022-05-19',
-        'P-0017'
-    );
-
-INSERT INTO
-    Booking_Details
-VALUES
-(
-        'B107',
-        'G07',
-        '501',
-        'S07',
-        '2022-05-19',
-        '2022-05-25',
-        'P-0018'
-    );
-
+--->Step -2
+--> Creating Rooms
 CREATE TABLE Rooms(
-    Room_No VARCHAR(40) REFERENCES Room_Type(Room_no),
-    Room_Type_ID VARCHAR(30),
+    Room_No VARCHAR(40) REFERENCES Room_Type(Room_no) NOT NULL ,
+    Room_Type_ID VARCHAR(30) NOT NULL,
 );
 
 INSERT INTO
@@ -142,12 +45,15 @@ INSERT INTO
 VALUES
     ('501', 'R05');
 
+--->Step-3
+---> Creating Room Types
+
 CREATE TABLE Room_Type(
-    Room_Type_ID VARCHAR(30),
-    Room_Type VARCHAR(30),
-    Room_no VARCHAR(30) PRIMARY KEY,
-    Price VARCHAR(30),
-    Floor_ID VARCHAR(30)
+    Room_Type_ID VARCHAR(30) NOT NULL,
+    Room_Type VARCHAR(30) NOT NULL,
+    Room_no VARCHAR(30) PRIMARY KEY NOT NULL,
+    Price VARCHAR(30) NOT NULL,
+    Floor_ID VARCHAR(30) NOT NULL
 );
 
 INSERT INTO
@@ -210,13 +116,16 @@ INSERT INTO
 VALUES
 ('R05', 'Presidential_Suit', '501', '20,000', '5');
 
+--->Step-4
+---> Creating Staff Details
+
 CREATE TABLE Staff_Details(
-    Staff_ID VARCHAR(25) PRIMARY KEY,
-    Staff_Name VARCHAR(25),
-    Staff_Gender VARCHAR(10),
-    Staff_Position VARCHAR(25),
-    staf_Contact_Number VARCHAR(30),
-    Staff_EmailID VARCHAR(25)
+    Staff_ID VARCHAR(25) PRIMARY KEY NOT NULL,
+    Staff_Name VARCHAR(25) NOT NULL,
+    Staff_Gender VARCHAR(10) NOT NULL,
+    Staff_Position VARCHAR(25) NOT NULL,
+    staf_Contact_Number VARCHAR(30) NOT NULL,
+    Staff_EmailID VARCHAR(25) NOT NULL
 );
 
 INSERT INTO
@@ -303,14 +212,19 @@ VALUES
         'biden@gmail.com'
     );
 
-CREATE TABLE Guest_Detials(
-    Guest_ID VARCHAR(25) PRIMARY KEY,
-    Guest_First_Name VARCHAR(25),
-    Guest_Last_Name VARCHAR(25),
-    Guest_Gender VARCHAR(25),
-    Guest_ID_Proof VARCHAR(25),
-    Guest_Email VARCHAR(30),
-    Guest_Address VARCHAR(30)
+
+
+--->Step-5
+---->Creating Guest  table 
+
+CREATE TABLE Guest_Details(
+    Guest_ID VARCHAR(25) PRIMARY KEY NOT NULL,
+    Guest_First_Name VARCHAR(25) NOT NULL,
+    Guest_Last_Name VARCHAR(25) NOT NULL,
+    Guest_Gender VARCHAR(25) NOT NULL,
+    Guest_ID_Proof VARCHAR(25) NOT NULL,
+    Guest_Email VARCHAR(30) NOT NULL,
+    Guest_Address VARCHAR(30) NOT NULL
 );
 
 INSERT INTO
@@ -404,11 +318,14 @@ VALUES
         'Shadhnagar,Hyderabad'
     );
 
+---->Step-6
+---->Creating Payment Details
+
 CREATE TABLE Payment_Details(
-    Payment_ID VARCHAR(25) PRIMARY KEY,
-    Payment_Type VARCHAR(25),
-    Amount VARCHAR(25),
-    Amount_in_Detail VARCHAR(30)
+    Payment_ID VARCHAR(25) PRIMARY KEY NOT NULL,
+    Payment_Type VARCHAR(25) NOT NULL,
+    Amount VARCHAR(25) NOT NULL, 
+    Amount_in_Detail VARCHAR(30) NOT NULL
 );
 
 INSERT INTO
@@ -445,3 +362,108 @@ INSERT INTO
     Payment_Details
 VALUES
     ('P-0018', 'Cash', '120,000', '20,000*6');
+
+
+--->Step-7
+---> Creating Booking Details
+
+    CREATE TABLE Booking_Details(
+    Booking_ID VARCHAR(10) PRIMARY KEY NOT NULL,
+    Guest_ID VARCHAR(25) REFERENCES Guest_Detials(Guest_ID) NOT NULL,
+    Room_No VARCHAR(40) REFERENCES Room_Type(Room_No) NOT NULL,
+    Staff_ID VARCHAR(25) REFERENCES Staff_Details(Staff_ID) NOT NULL,
+    Arrival_Date DATE NOT NULL,
+    Departurte_Date DATE NOT NULL,
+    Payment_ID VARCHAR(25) REFERENCES Payment_Details(Payment_ID) NOT NULL
+);
+
+INSERT INTO
+    Booking_Details
+VALUES
+(
+        'B101',
+        'G01',
+        '101',
+        'S02',
+        '2022-05-15',
+        '2022-05-17',
+        'P-0012'
+    );
+
+INSERT INTO
+    Booking_Details
+VALUES
+(
+        'B102',
+        'G02',
+        '201',
+        'S04',
+        '2022-05-16',
+        '2022-05-18',
+        'P-0013'
+    );
+
+INSERT INTO
+    Booking_Details
+VALUES
+(
+        'B103',
+        'G03',
+        '401',
+        'S06',
+        '2022-05-16',
+        '2022-05-20',
+        'P-0014'
+    );
+
+INSERT INTO
+    Booking_Details
+VALUES
+(
+        'B104',
+        'G04',
+        '202',
+        'S01',
+        '2022-05-17',
+        '2022-05-22',
+        'P-0015'
+    );
+
+INSERT INTO
+    Booking_Details
+VALUES
+(
+        'B105',
+        'G05',
+        '301',
+        'S04',
+        '2022-05-17',
+        '2022-05-19',
+        'P-0016'
+    );
+
+INSERT INTO
+    Booking_Details
+VALUES
+(
+        'B106',
+        'G06',
+        '402',
+        'S03',
+        '2022-05-18',
+        '2022-05-19',
+        'P-0017'
+    );
+
+INSERT INTO
+    Booking_Details
+VALUES
+(
+        'B107',
+        'G07',
+        '501',
+        'S07',
+        '2022-05-19',
+        '2022-05-25',
+        'P-0018'
+    );
