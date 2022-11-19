18036645f44bb7e139883c232aaea2381bda4be7
pavankumar095 <pillipavankumar095@gmail.com>
2022-11-19 18:17:27 +0530

    task uploaded

--- Week5_SQL/pavankumar/queries.sql
+++ Week5_SQL/pavankumar/queries.sql
@@ -1,4 +1,4 @@
-1.fetch all nurse names who are assigned to room 3
+//1.fetch all nurse names who are assigned to room 3
 
 hospital_management=# select n.name,n.id from nurses n,room_nurse_mapping r where n.id=r.nurse_id and r.room_id='3';
   name   | id
@@ -7,7 +7,7 @@ hospital_management=# select n.name,n.id from nurses n,room_nurse_mapping r wher
  rajesh  |  6
  prudhvi | 10
 
-2.select the doctor who is having highest patients
+//2.select the doctor who is having highest patients
 
 hospital_management=# select  d.doctor_id,count(inward_id) from doctor_inwards d group by d.doctor_id order by count(inward_id) desc;
  doctor_id | count
@@ -22,10 +22,11 @@ hospital_management=# select  d.doctor_id,count(inward_id) from doctor_inwards d
          5 |     1
 
 
-3.Write a query to get all the patient details who have suffered from fever?
+//3.Write a query to get all the patient details who have suffered from fever?
 
+select * from patients,inwards where patients.id=inwards.patient_id and inwards.disease='fever';
 
-4. Write a query to get inwards whose bill is more than 50000?
+//4. Write a query to get inwards whose bill is more than 50000?
 
 hospital_management=# select patient_id,total_bill from inwards where total_bill > 50000;
  patient_id | total_bill
@@ -43,14 +44,14 @@ hospital_management=# select patient_id,total_bill from inwards where total_bill
          14 |      80000
          15 |      60000
 
-5. Write a query to get all the doctors who don't even have one inward?
-
+//5. Write a query to get all the doctors who don't even have one inward?
+//Another approach is exists or not exists
 hospital_management=# select d.id,d.name from doctors d left join doctor_inwards di ON di.doctor_id=d.id where di.doctor_id is null;
  id |       name
 ----+------------------
   9 | Dr. Mangathayaru
 
-6. Write a query to get the list of rooms which have vacant beds?
+//6. Write a query to get the list of rooms which have vacant beds?
 
 hospital_management=# select r.id,r.status from rooms r where r.status='empty';
  id | status
@@ -58,4 +59,30 @@ hospital_management=# select r.id,r.status from rooms r where r.status='empty';
   3 | empty
   4 | empty
 
-
+//7. Write a query to get list nurses with reported docters.
+hospital_management=# select d.doctor_id,r.nurse_id from doctor_inwards d,room_nurse_mapping r ,inwards i where i.id=d.inward_id and r.room_id=i.room_id;
+ doctor_id | nurse_id
+-----------+----------
+         1 |        2
+         1 |        1
+         1 |        2
+         1 |        1
+         2 |        2
+         2 |        1
+         2 |        9
+         2 |        4
+         2 |        3
+         3 |        9
+         3 |        4
+         3 |        3
+         3 |        9
+         3 |        4
+         3 |        3
+         4 |       10
+         4 |        6
+         4 |        5
+         4 |       10
+         4 |        6
+         4 |        5
+         5 |        8
+         5 |        7
