5857e94cd71858e4b210df0441a6636fcc6d22e8
pavankumar095 <pillipavankumar095@gmail.com>
2022-12-01 10:53:10 +0530

    changes updated

--- week6_task/pavan kumar/Apis/createinward.php
+++ week6_task/pavan kumar/Apis/createinward.php
@@ -31,6 +31,16 @@ if (!array_key_exists('disease_id', $_POST)){
 }
 
 $pdo = getDbConnection();
+$statement = $pdo->prepare("select * from inwards where patient_id=? and disease_id");
+$statement->execute([$_POST['patient_id'],$_POST['disease_id']]);
+$resultSet = $statement->fetchAll(PDO::FETCH_ASSOC);
+if (count($resultSet) == 2) {
+    $response['status'] = false;
+    $response['message'] = "only one inward allowed under one disease";
+    echo json_encode($response);
+    return;
+}
+
 $sql = "INSERT INTO inwards(patient_id,doctor_id,room_id,disease_id) VALUES (?,?,?,?)";
 $stmt = $pdo->prepare($sql);
 $stmt->execute([$_POST['patient_id'], $_POST['doctor_id'], $_POST['room_id'], $_POST['disease_id']]);
@@ -43,3 +53,4 @@ $response['message'] = "data saved successfully";
 $response['data'] = $resultSet;
 echo json_encode($response);
 return;
+
